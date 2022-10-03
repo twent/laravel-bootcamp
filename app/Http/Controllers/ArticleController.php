b39b863cb5a8bbf39e192e9848037f9999c70d3a
twent <twentprod@gmail.com>
2022-10-03 20:47:46 +0300

    Article controller was created

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
+use App\Models\Article;
+use Inertia\Inertia;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function index()
+    {
+        $articles = Article::with('user:id,name')->latest()->get();
+
+        return Inertia::render('Articles/Index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreArticleRequest  $request
+     * @return \Illuminate\Routing\Redirector
+     */
+    public function store(StoreArticleRequest $request)
+    {
+        $formData = $request->validated();
+        $request->user()->articles()->create($formData);
+
+        return redirect(route('dashboard.articles.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateArticleRequest  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(UpdateArticleRequest $request, Article $article)
+    {
+        $this->authorize('update', $article);
+
+        $article->updateOrFail($request->validated());
+
+        return redirect(route('dashboard.articles.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+}
