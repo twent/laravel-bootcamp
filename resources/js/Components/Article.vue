4d969fb9ee3881f9978e97466a994ae9fd8e2d05
twent <twentprod@gmail.com>
2022-10-03 20:49:01 +0300

    edit functionality for Article Vue component was added

--- resources/js/Components/Article.vue
+++ resources/js/Components/Article.vue
@@ -1,5 +1,19 @@
 <script setup>
-defineProps(['article']);
+import Dropdown from '@/Components/Dropdown.vue';
+import TextInput from '@/Components/TextInput.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue';
+
+const props = defineProps(['article']);
+
+const form = useForm({
+    title: props.article.title,
+    text: props.article.text,
+});
+
+const editing = ref(false);
 </script>
 
 <template>
@@ -12,10 +26,38 @@ defineProps(['article']);
                 <div>
                     <span class="text-gray-800">{{ article.user.name }}</span>
                     <small class="ml-2 text-sm text-gray-600">{{ article.created_at }}</small>
+                    <small v-if="article.created_at !== article.updated_at" class="text-sm text-gray-600"> &middot; edited</small>
                 </div>
+                <Dropdown v-if="article.user.id === $page.props.auth.user.id">
+                    <template #trigger>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </template>
+                    <template #content>
+                        <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
+                            Edit
+                        </button>
+                    </template>
+                </Dropdown>
             </div>
-            <h3 class="mt-4 text-2xl text-indigo-500">{{ article.title }}</h3>
-            <p class="mt-2 text-lg text-gray-900">{{ article.text }}</p>
+            <form v-if="editing" @submit.prevent="form.put(route('dashboard.articles.update', article.id), { onSuccess: () => editing = false })">
+                <TextInput id="title" type="text" placeholder="Title" class="mb-2 block w-full" v-model="form.title" required autofocus autocomplete="false" />
+                <InputError :message="form.errors.title" class="mt-2" />
+
+                <textarea v-model="form.text" class="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+                <InputError :message="form.errors.text" class="mt-2" />
+                <div class="space-x-2">
+                    <PrimaryButton class="mt-4">Save</PrimaryButton>
+                    <button class="mt-4" @click="editing = false; form.reset(); form.clearErrors()">Cancel</button>
+                </div>
+            </form>
+            <div v-else>
+                <h3 class="mt-4 text-2xl text-indigo-500">{{ article.title }}</h3>
+                <p class="mt-4 text-lg text-gray-900">{{ article.text }}</p>
+            </div>
         </div>
     </div>
 </template>
