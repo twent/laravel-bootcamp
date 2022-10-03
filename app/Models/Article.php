d86b6b5b3c95c298e4eef98c3b9e069f786bf74a
twent <twentprod@gmail.com>
2022-10-03 19:14:20 +0300

    dates getters was added

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,6 +17,30 @@ class Article extends Model
         'title',
         'text',
     ];
+
+    /**
+     * CreatedAt getter
+     *
+     * @return Attribute
+     */
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::createFromDate($value)->diffForHumans(),
+        );
+    }
+
+    /**
+     * UpdatedAt getter
+     *
+     * @return Attribute
+     */
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::createFromDate($value)->diffForHumans(),
+        );
+    }
 
     /**
      * Relation to User
