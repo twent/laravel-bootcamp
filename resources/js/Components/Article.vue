ad97eb1c7e82916576cdab8dee5e11447b0b5f87
twent <twentprod@gmail.com>
2022-10-03 20:56:16 +0300

    link for delete article was added

--- resources/js/Components/Article.vue
+++ resources/js/Components/Article.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 import TextInput from '@/Components/TextInput.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -40,6 +41,9 @@ const editing = ref(false);
                         <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
                             Edit
                         </button>
+                        <DropdownLink as="button" :href="route('dashboard.articles.destroy', article.id)" method="delete">
+                            Delete
+                        </DropdownLink>
                     </template>
                 </Dropdown>
             </div>
