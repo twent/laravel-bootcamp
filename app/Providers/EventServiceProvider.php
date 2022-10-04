321582d13e6e25c9040cd1fa5c42b65b2d38d1c7
twent <twentprod@gmail.com>
2022-10-04 21:59:52 +0300

    register processing in event provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ArticleCreated;
+use App\Listeners\SendArticleCreatedNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -15,6 +17,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+        ArticleCreated::class => [
+            SendArticleCreatedNotification::class,
+        ],
+
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
