14789880e6156c8aecff7eb47dbb48ded54ca234
twent <twentprod@gmail.com>
2022-10-04 21:48:59 +0300

    new article notification was created

---
+++ app/Notifications/NewArticle.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Article;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewArticle extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(public Article $article)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject("New Article from {$this->article->user->name}")
+            ->greeting("New Chirp from {$this->article->user->name}")
+            ->line(Str::limit($this->article->message, 50))
+            ->action('Go to Articles', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
