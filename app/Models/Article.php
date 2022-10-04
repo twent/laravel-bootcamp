75d480650dc409d946efcecabd3bf993275fd46f
twent <twentprod@gmail.com>
2022-10-04 21:56:54 +0300

    article created event was added

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ArticleCreated;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,6 +17,10 @@ class Article extends Model
     protected $fillable = [
         'title',
         'text',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ArticleCreated::class,
     ];
 
     /**
